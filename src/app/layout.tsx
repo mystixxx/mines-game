8a2d84e0f26f1634782bff7beaa39d06cb2d33da
mystixxx <okejhere@gmail.com>
2025-04-18 15:34:39 +0200

    add metadata, and viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,21 @@ import "@/styles/globals.css";
 import "@/styles/typography.css";
 import { Inter } from "next/font/google";
 import { Toaster } from "react-hot-toast";
+import type { Metadata } from "next";
 
 const inter = Inter({
 	subsets: ["latin"],
 	variable: "--font-inter",
 	display: "swap",
 });
+
+export const metadata: Metadata = {
+	title: "Mines Game",
+	viewport: "initial-scale=1, width=device-width",
+	icons: {
+		icon: "/favicon.ico",
+	},
+};
 
 export default function RootLayout({
 	children,
