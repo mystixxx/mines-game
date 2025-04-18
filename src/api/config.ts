ae8e3752a96ae40d2278531315f0f6f85fec7302
mystixxx <okejhere@gmail.com>
2025-04-18 15:24:23 +0200

    .env file

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,6 +1,10 @@
 // API Configuration
+if (!process.env.NEXT_PUBLIC_API_BASE_URL) {
+  throw new Error('NEXT_PUBLIC_API_BASE_URL is not defined in .env file');
+}
+
 export const API_CONFIG = {
-  BASE_URL: "https://wallet-demo-435v2.ondigitalocean.app",
+  BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
   HEADERS: {
     "X-API-KEY":
       "TxDONKnVZslnzVq2kZHoxQt9715WTZHyYVcbij0nQQTKHKIFVYmRGZtOWUP8SnEp",
